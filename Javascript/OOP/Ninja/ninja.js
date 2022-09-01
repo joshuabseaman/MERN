96c3713164aad340295c4d0a5e6519862f3ed013
joshuabseaman <joshuabseaman@gmail.com>
2022-09-01 11:09:29 -0600

    Ninja

---
+++ Javascript/OOP/Ninja/ninja.js
@@ -0,0 +1,23 @@
+class Ninja {
+    constructor(name, health) {
+        this.name = name
+        this.health = health
+        this.speed = 3
+        this.strength = 3
+    }
+    sayName() {
+        console.log(`Ninja's name is ${this.name}.`);
+    }
+    showStats() {
+        console.log(`Ninja's Stats: ${this.name}, ${this.strength}, ${this.speed}, ${this.health}.`);
+    }
+    drinkSake() {
+        this.health += 10;
+    }
+}
+
+const ninja1 = new Ninja("Hyabusa", 50);
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
+ninja1.showStats();
